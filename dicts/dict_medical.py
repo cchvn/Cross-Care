3d41d43fbf531cd347cbe8653c6d8be1cb3e67d6
shan <shawnchen23@gmail.com>
2023-12-08 13:56:14 -0500

    updating keyword on med among normalization data

--- dicts/dict_medical.py
+++ dicts/dict_medical.py
@@ -495,7 +495,596 @@ medical_keywords_dict = {
 "Ovarian Cancers",
 "Cancer of Ovary",
 "Cancer of the Ovary"
-]}
+],
+
+#3545003.0 
+"3545003.0": ['stage 1 diastolic dysfunction', 'diastolic dysfunction'], 
+
+#3716002.0 
+"3716002.0": ['goiter'], 
+
+#3723001.0 
+"3723001.0": ['severe arthritis', 'arthritis', 'Arthritis', 'ARTHRITIS', 'inflammation of my joints', 'sever arthritic condition'], 
+
+#4308002.0 
+"4308002.0": ['Repetitive stress syndrome'], 
+
+#6142004.0 
+"6142004.0": ['flu', 'flu bug'], 
+
+#6471006.0 
+"6471006.0": ['thoughts about suicide'], 
+
+#7973008.0 
+"7973008.0": ['visual anomalies'], 
+
+#8357008.0 
+"8357008.0": ['loss of sex drv'], 
+
+#9126005.0 
+"9126005.0": ['spotting problems'], 
+
+#9829001.0 
+"9829001.0": ['perforated ulcer'], 
+
+#10743008.0 
+"10743008.0": ['IBS'], 
+
+#11072008.0 
+"11072008.0": ['abdominal problems'], 
+
+#11381005.0 
+"11381005.0": ['acne breakouts on my face and neck'], 
+
+#11654001.0 
+"11654001.0": ['Achilles peritendonitis', 'Achilles Tendonsitis', 'Severe achilles tendonitis'], 
+
+#13746004.0 
+"13746004.0": ['bipolar disorder'], 
+
+#13791008.0 
+"13791008.0": ['DEBILITATED', 'debilitated', 'debilitating', 'Debilitating'], 
+
+#14304000.0 
+"14304000.0": ['Thyroid disease'], 
+
+#14669001.0 
+"14669001.0": ['acute kidney failure'], 
+
+#15188001.0 
+"15188001.0": ['Deafness'], 
+
+#18193002.0 
+"18193002.0": ['Hyprochondria'], 
+
+#18718003.0 
+"18718003.0": ['gum trouble'], 
+
+#21134002.0 
+"21134002.0": ['disability'], 
+
+#22193007.0 
+"22193007.0": ['severe osteoarthritis in the hands'], 
+
+#22298006.0 
+"22298006.0": ['heart attack', 'miocardial infarction', 'Myocardial Infarction', 'HEART ATTACK', 'Heart attack'], 
+
+#23502006.0 
+"23502006.0": ['Lyme disease', 'Lyme Disease'], 
+
+#23919004.0 
+"23919004.0": ['labyrinthitis'], 
+
+#24693007.0 
+"24693007.0": ['fibromyalgia', 'Fibromyalgia', 'fibromalgia', 'fibromalgyia', 'FIBROMYALGIA'], 
+
+#24700007.0 
+"24700007.0": ['Multiple Sclerosis'], 
+
+#25458004.0 
+"25458004.0": ['acute gastritis'], 
+
+#26544005.0 
+"26544005.0": ['INFLAMMATORY muscle weakness & degeneration'], 
+
+#26889001.0 
+"26889001.0": ['muscle inflammation'], 
+
+#26929004.0 
+"26929004.0": ["Alzheimer's", "alzheimer's", 'alzheimers'], 
+
+#29384001.0 
+"29384001.0": ['gastric problems'], 
+
+#30811009.0 
+"30811009.0": ['esofogual uclers'], 
+
+#30989003.0 
+"30989003.0": ['Major sorness in knees'], 
+
+#31384009.0 
+"31384009.0": ['Polymyositis', 'polymyositis'], 
+
+#32398004.0 
+"32398004.0": ['bronchitis', 'bronhitis'], 
+
+#32798002.0 
+"32798002.0": ["symptoms of parkinson's disease", "Parkinson's symptoms", "symptoms of Parkison's disease"], 
+
+#32914008.0 
+"32914008.0": ['restless legs syndrome (rls)', 'restless leg syndrome', 'Restless Leg Syndrome', 'attack of Restless Leg Syndrome'], 
+
+#33308003.0 
+"33308003.0": ['back problems'], 
+
+#34840004.0 
+"34840004.0": ['tendonitis', 'flared up tendenitis', 'Tendonitis', 'tendinitis'], 
+
+#35489007.0 
+"35489007.0": ['big time depression', 'very serious depression', 'suicidal depression'], 
+
+#36971009.0 
+"36971009.0": ['sinus infection'], 
+
+#37340000.0 
+"37340000.0": ['MND'], 
+
+#38936003.0 
+"38936003.0": ['bp problems'], 
+
+#39400004.0 
+"39400004.0": ['permanent nerve damage', 'Liver Injury'], 
+
+#39621005.0 
+"39621005.0": ['gall bladder disease'], 
+
+#40095003.0 
+"40095003.0": ['Kidney Injury'], 
+
+#40733004.0 
+"40733004.0": ['infection', 'infections', 'severe infections'], 
+
+#40930008.0 
+"40930008.0": ['hypothyroidism'], 
+
+#41982003.0 
+"41982003.0": ['male menopause'], 
+
+#42345000.0 
+"42345000.0": ['polyneuropathy'], 
+
+#42399005.0 
+"42399005.0": ['kidney failure', 'Kidney failure', 'kidney malfunction', 'renal failure'], 
+
+#43116000.0 
+"43116000.0": ['eczema'], 
+
+#43364001.0 
+"43364001.0": ['Gasto discomfort'], 
+
+#44054006.0 
+"44054006.0": ['type2 diabetes', 'Type II Diabetes', 'Type II diabetic', 'type 2 diabetes'], 
+
+#44695005.0 
+"44695005.0": ['paralysis'], 
+
+#45352006.0 
+"45352006.0": ['spazms'], 
+
+#46635009.0 
+"46635009.0": ['type one diabetic', 'type 1 Diabetes', 'type 1 diabetes'], 
+
+#47004009.0 
+"47004009.0": ['speech problems', 'speech difficulties like pronunciation'], 
+
+#48661000.0 
+"48661000.0": ['peritonitis'], 
+
+#49049000.0 
+"49049000.0": ['Parkinson', "parkinson's disease", "Parkinson's", "parkinson's", 'Parkinsons'], 
+
+#49601007.0 
+"49601007.0": ['cardiovascular disease'], 
+
+#50829006.0 
+"50829006.0": ['learning problems'], 
+
+#52448006.0 
+"52448006.0": ['dementia'], 
+
+#52702003.0 
+"52702003.0": ['chronic fatigue syndrome'], 
+
+#53741008.0 
+"53741008.0": ['coronary artery disease', 'Coronary Artery Disease'], 
+
+#54150009.0 
+"54150009.0": ['upper respiratory infections'], 
+
+#55300003.0 
+"55300003.0": ['muscle spasm'], 
+
+#56265001.0 
+"56265001.0": ['heart damage', 'heart problems', 'heart disease', 'coronary disease', 'HEART DISEASE', 'coronary problems', 'severe heart disease', 'Heart disease'], 
+
+#56317004.0 
+"56317004.0": ['alopecia', 'Alopecia'], 
+
+#56925008.0 
+"56925008.0": ['sexual dysfunction'], 
+
+#57182000.0 
+"57182000.0": ['nerve damage', 'permanent nerve damage to my feet'], 
+
+#57406009.0 
+"57406009.0": ['carpal tunnel syndrome'], 
+
+#57676002.0 
+"57676002.0": ['arthritis pain', 'jointaches', 'finger joint pain similar to arthritis', 'feet joint pain similar to arthritis'], 
+
+#59927004.0 
+"59927004.0": ['liver failure', 'liver disfunction'], 
+
+#60032008.0 
+"60032008.0": ['problems with concentration'], 
+
+#60862001.0 
+"60862001.0": ['tinititis', 'tinnitus', 'tinnitis', 'severe tintitinitis (ringing in ears)', 'Tinnitus', 'tinnitus in my left ear', 'tinnitus (left ear)', 'problems with tinnitus'], 
+
+#61462000.0 
+"61462000.0": ['malaria'], 
+
+#61599003.0 
+"61599003.0": ['phlebitis'], 
+
+#62315008.0 
+"62315008.0": ['diaharrea', 'diarea', 'diahrrea'], 
+
+#63102001.0 
+"63102001.0": ['vision problems', 'visual deficits', 'eyesight problems'], 
+
+#64226004.0 
+"64226004.0": ['collitis', 'colitis'], 
+
+#64766004.0 
+"64766004.0": ['ulcerative colitis'], 
+
+#66383009.0 
+"66383009.0": ['gum infections'], 
+
+#67362008.0 
+"67362008.0": ['aortic aneurysem'], 
+
+#68172002.0 
+"68172002.0": ['tendinopathy'], 
+
+#68449006.0 
+"68449006.0": ['severe arthritis symptoms in hip joints'], 
+
+#68566005.0 
+"68566005.0": ['urinery tract infection', 'severe urinary tract infections'], 
+
+#68962001.0 
+"68962001.0": ['muscle achs'], 
+
+#69322001.0 
+"69322001.0": ['Psychosis'], 
+
+#69896004.0 
+"69896004.0": ['rheumatoid arthritis', 'rheumatoid arthriitis', 'Rhuematoid Arthritis', 'Rheumatoid Arthtitis'], 
+
+#70076002.0 
+"70076002.0": ['aggrivates my Rhinitis', 'rhinitis'], 
+
+#73211009.0 
+"73211009.0": ['diabetes', 'Diabetes'], 
+
+#73442001.0 
+"73442001.0": ['Stevens Johnson Syndrome'], 
+
+#74732009.0 
+"74732009.0": ['mental illness'], 
+
+#75694006.0 
+"75694006.0": ['acute pancreatic attack', 'pancreatitis', 'Pancreatitis'], 
+
+#76376003.0 
+"76376003.0": ['adenomyosis'], 
+
+#77547008.0 
+"77547008.0": ['degenerative back disease'], 
+
+#80690008.0 
+"80690008.0": ['neurological problems', 'neurodegeneration'], 
+
+#80967001.0 
+"80967001.0": ['tooth decay'], 
+
+#81308009.0 
+"81308009.0": ['brain problems'], 
+
+#81498004.0 
+"81498004.0": ['bursitis in the hips'], 
+
+#82639001.0 
+"82639001.0": ['PMS symptoms'], 
+
+#82991003.0 
+"82991003.0": ['aching (like the flu)'], 
+
+#83901003.0 
+"83901003.0": ["sorgren's syndrome"], 
+
+#84017003.0 
+"84017003.0": ['burscitis', 'bursitis'], 
+
+#85828009.0 
+"85828009.0": ['auto immune diseases'], 
+
+#86044005.0 
+"86044005.0": ['ALS', 'Amyotrophic lateral sclerosis', 'amyotrophic lateral sclerosis', 'als'], 
+
+#87486003.0 
+"87486003.0": ['aphasia', 'language and learning ability loss including spoken and written aphasia and inability to read'], 
+
+#88092000.0 
+"88092000.0": ['muscle wasting in the shoulders', 'muscle wasting in the arms', 'muscles wasting', 'Muscle wasting', 'muscle wasting', 'muscle loss', 'rhabdomyolysis/muscle loss', 'muscle disintegration/degeneration'], 
+
+#90708001.0 
+"90708001.0": ['KIDNEY TROUBLES', 'kidney problems', 'KIDNEY PROBLEMS'], 
+
+#95891005.0 
+"95891005.0": ['flu symptons', 'i felt like I always had the flu', 'felt like I had the flu', 'felt like I was coming down with flu'], 
+
+#102480002.0 
+"102480002.0": ['felt like a truck hit me'], 
+
+#118940003.0 
+"118940003.0": ['nervous system problems'], 
+
+#128601007.0 
+"128601007.0": ['infection in her lungs'], 
+
+#128609009.0 
+"128609009.0": ['brain aneurysm'], 
+
+#129103003.0 
+"129103003.0": ['endometriosis', 'Endometriosis'], 
+
+#129565002.0 
+"129565002.0": ['muscle problems', 'severe myopathy', 'myopathy', 'muscle problem', 'myopathies', 'muscle problems legs', 'Extreme muscle problems'], 
+
+#131148009.0 
+"131148009.0": ['Bleeding problems', 'bleeding problems', 'bleeding problem'], 
+
+#134407002.0 
+"134407002.0": ['chronic backache'], 
+
+#161156004.0 
+"161156004.0": ['language problems'], 
+
+#161891005.0 
+"161891005.0": ['backache'], 
+
+#162076009.0 
+"162076009.0": ['gastritis', 'gastro problem', 'gastro effects', 'GI difficulties', 'glossitis'], 
+
+#162315007.0 
+"162315007.0": ['obsessively focused on morbid'], 
+
+#186748004.0 
+"186748004.0": ['active parvovirus infection'], 
+
+#191736004.0 
+"191736004.0": ['Bad OCD'], 
+
+#193000002.0 
+"193000002.0": ['Temporal Lobe epilepsy'], 
+
+#193093009.0 
+"193093009.0": ["Bell's palsy"], 
+
+#193462001.0 
+"193462001.0": ['insomnia', 'insomia', 'Insomnia', 'Mild insomnia', 'severe insomnia', 'INSOMNIA', 'Insomia', 'Insomina', 'insomina'], 
+
+#193570009.0 
+"193570009.0": ['cataract'], 
+
+#195967001.0 
+"195967001.0": ['asthma'], 
+
+#200936003.0 
+"200936003.0": ['lupus', 'Lupus'], 
+
+#202852009.0 
+"202852009.0": ['tendonitis in my shoulders'], 
+
+#202882003.0 
+"202882003.0": ['plantar fasciitis', 'plantar fasciatis', 'plantar fasciitis of my left foot', 'plantar fasciitis in my feet', 'Severe plantar faciitis'], 
+
+#213257006.0 
+"213257006.0": ['feeling horrible like a severe flu just hit me'], 
+
+#214264003.0 
+"214264003.0": ['leathargy'], 
+
+#233604007.0 
+"233604007.0": ['pneumonia'], 
+
+#235856003.0 
+"235856003.0": ['liver disease'], 
+
+#236620008.0 
+"236620008.0": ['bladder infection', 'BLADDER INFECTION'], 
+
+#237602007.0 
+"237602007.0": ['Metabolic syndrome'], 
+
+#238539001.0 
+"238539001.0": ['Eczema on hands'], 
+
+#239873007.0 
+"239873007.0": ['severe osteoarthritis in the knees'], 
+
+#239965002.0 
+"239965002.0": ['tendonitis in elbows', 'severe and chronic tendonitis in my elbows'], 
+
+#240131006.0 
+"240131006.0": ['Rhabdomyolosis', 'rhabdomyolosis', 'rhabdomyolisis'], 
+
+#247592009.0 
+"247592009.0": ['short term memory problems', 'Terrible short term memory problems', 'Short term memory problems', 'working memory and short-term memory loss', 'short term memory dysfunction'], 
+
+#250084002.0 
+"250084002.0": ['severn calf cramps'], 
+
+#267045008.0 
+"267045008.0": ['major gastrointestinal problems', 'Gastrointestinal problems'], 
+
+#267953003.0 
+"267953003.0": ['Leg joint pain similar to arthritis'], 
+
+#271587009.0 
+"271587009.0": ['stiffness similar to arthritis'], 
+
+#271596009.0 
+"271596009.0": ['mental distress'], 
+
+#271767006.0 
+"271767006.0": ['foot skin flaked off like dermatitis', 'ankle skin flaked off like dermatitis'], 
+
+#271832001.0 
+"271832001.0": ['gas pains around my abdomen', 'gas pains in back'], 
+
+#278040002.0 
+"278040002.0": ['hair loss', 'Hair loss', 'hairloss', 'Hair loss (Diffuse Alopecia)'], 
+
+#280816001.0 
+"280816001.0": ['facial paralysis'], 
+
+#281794004.0 
+"281794004.0": ['viral upper respiratory infection'], 
+
+#302226006.0 
+"302226006.0": ['peripheral neuropathy in arms', 'peripheral neuropathy in feet and legs', 'peripheral neuropathy in my hands', 'toe neuropathy', 'peripheral neuropathy', 'Periphiral neuropathy', 'Peripheral Neuropathy', 'periphael neuropathy'], 
+
+#307496006.0 
+"307496006.0": ['bouts of diverticulitis', 'diverticulitis'], 
+
+#308925008.0 
+"308925008.0": ['digestion problems'], 
+
+#314983004.0 
+"314983004.0": ['Kidney function became a problem'], 
+
+#315642008.0 
+"315642008.0": ['coming down with the flu'], 
+
+#363058009.0 
+"363058009.0": ['degenative muscle problem'], 
+
+#363346000.0 
+"363346000.0": ['CANCER', 'cancer'], 
+
+#363406005.0 
+"363406005.0": ['Colin cancer'], 
+
+#367391008.0 
+"367391008.0": ['malaisse'], 
+
+#370143000.0 
+"370143000.0": ['major depression'], 
+
+#371081002.0 
+"371081002.0": ['severe arthritis symptoms in knees'], 
+
+#371405004.0 
+"371405004.0": ['eye trouble', 'eye problems', 'Eye problem'], 
+
+#371632003.0 
+"371632003.0": ['coma'], 
+
+#386033004.0 
+"386033004.0": ['nerve problems in shoulder', 'neuropathy', 'neurophathy', 'NEUROPATHY', 'Neuropathy', 'neurophaty', 'Severe neuropathy'], 
+
+#386806002.0 
+"386806002.0": ['cognitive impairments', 'Cognitive disfunction'], 
+
+#386807006.0 
+"386807006.0": ['memory problems', 'memory failure', 'memory problem', 'memeory problems', 'MEMORY problems', 'problems with memory'], 
+
+#396275006.0 
+"396275006.0": ['osteoarthritis', 'signs of osteoarthritis'], 
+
+#397540003.0 
+"397540003.0": ['vision impaired', 'Sight loss'], 
+
+#397803000.0 
+"397803000.0": ['erectile disfunction'], 
+
+#397825006.0 
+"397825006.0": ['stomache ulcers'], 
+
+#399068003.0 
+"399068003.0": ['prostate cancer'], 
+
+#399122003.0 
+"399122003.0": ['swallowing problems'], 
+
+#399269003.0 
+"399269003.0": ['joint problems'], 
+
+#402234004.0 
+"402234004.0": ['Eczema on feet'], 
+
+#409622000.0 
+"409622000.0": ['renal and respiratory failure'], 
+
+#423849004.0 
+"423849004.0": ['left leg iliotial band syndrome'], 
+
+#425772008.0 
+"425772008.0": ['tendonitisis in heels'], 
+
+#428061005.0 
+"428061005.0": ['brain cancer'], 
+
+#428281000.0 
+"428281000.0": ['bone cancer'], 
+
+#429040005.0 
+"429040005.0": ['ulcers'], 
+
+#429340002.0 
+"429340002.0": ['muscle injury'], 
+
+#429360005.0 
+"429360005.0": ['tendonitis in my knees'], 
+
+#440630006.0 
+"440630006.0": ['irritable bowel syndrome of the sigmoid area and descending colon with obstipation and constipation'], 
+
+#443694000.0 
+"443694000.0": ['TYPE II DIABETES BECAME UNCONTROLLED'], 
+
+#443892003.0 
+"443892003.0": ['neuroma'], 
+
+#444312002.0 
+"444312002.0": ['bleeding in my abdomen after coughing and rupturing a blood vessel'], 
+
+#448589005.0 
+"448589005.0": ['arthritis like symptoms in hands'], 
+
+#609599003.0 
+"609599003.0": ['Neuropothy in right hand', 'Neuropothy in right arm'], 
+
+#609600000.0 
+"609600000.0": ['neuropthy in both feet', 'neuropathy in left foot', 'leg neuropathy'], 
+
+#698247007.0 
+"698247007.0": ['arrhythmia', 'arrythmia']
+}
 
 medical_keywords_dict = {key.lower(): [word.lower() for word in value] 
 for key, value in medical_keywords_dict.items()}
